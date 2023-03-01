960af0a4871bf1b9a4063a912198adf540ea5c1d
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2023-03-01 17:05:22 +0300

    fixing dependencies 3

--- wavetransformer/models/__init__.py
+++ wavetransformer/models/__init__.py
@@ -2,11 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from .wave_transformer_3 import WaveTransformer3
-from .wave_transformer_8 import WaveTransformer8
-from .wave_transformer_10 import WaveTransformer10
 __author__ = 'An Tran'
 __docformat__ = 'reStructuredText'
-__all__ = ['WaveTransformer3', 
-           'WaveTransformer8',
-           'WaveTransformer10']
+__all__ = ['WaveTransformer3']
 # EOF
