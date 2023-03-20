fd2e9ebf99fe6278fbe584504de0df3e86493e8a
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2023-03-20 21:27:33 +0300

    dcase-2020 -> dcase-2022

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+import shutil
+import os
+
+from AudioCaption.captioning.pytorch_runners.inference_waveform import inference
+
+
+def cycle_inference(audio_dir, model_path):
+    def iteration(filename, filename_ix):
+        print(f"Trying to infer on {filename}")
+        cap = inference(
+            os.path.join(audio_dir, filename),
+            f"{filename_ix}.json", 
+            model_path
+        )
+        return cap
+
+    filenames = os.listdir(audio_dir)
+    filenames = list(filter(lambda file: file.split(".")[-1] in ['wav', 'mp3', 'flac'], filenames))
+    caps = []
+
+    if isinstance(filenames, list):
+        for filename_ix, filename in enumerate(filenames):
+            caps.append(iteration(filename, filename_ix))
+    else:
+        caps.append(iteration(filenames, 0))
+    print(f"caps: {caps}")
+
+
+if __name__ == "__main__":
+    audio_dir = '/home/ilia/projects/audio-captioning/AudioCaption/audiofiles'
+    model_path = "/home/ilia/projects/audio-captioning/AudioCaption/experiments/clotho_v2/train_val/TransformerModel/cnn14rnn_trm/seed_1/swa.pth"
+    cycle_inference(audio_dir, model_path)
--- wavetransformer/models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .wave_transformer_3 import WaveTransformer3
-__author__ = 'An Tran'
-__docformat__ = 'reStructuredText'
-__all__ = ['WaveTransformer3']
-# EOF
